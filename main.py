570e2a7980f7bc728ac2069d8709e20115891696
masu51 <ljashoi@yandexlyceum.ru>
2022-03-07 17:57:44 +0300

    ДОбавил еще одну строку текста

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import os
 
 from flask import Flask
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
 
 @app.route("/")
 def index():
     result = """<h1>Hello world</h1>
-    <p> Пример запуска приложения в Heroku</P>
+    <p> Пример запуска приложения в Heroku 123</P>
+    <p> ещё одна строка текста</p>
     """
     return result
 
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
+    app.run()
